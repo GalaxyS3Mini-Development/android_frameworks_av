f7be1a4105cfed8dff02f64e1490b6ce67ad507f
Shilin Victor <chrono.monochrome@gmail.com>
2018-08-20 22:57:43 +0300

    av: Revert mGraphicBufferEnabled

--- media/libstagefright/omx/include/media/stagefright/omx/OMXNodeInstance.h
+++ media/libstagefright/omx/include/media/stagefright/omx/OMXNodeInstance.h
@@ -164,7 +164,6 @@ private:
         kSecureBufferTypeNativeHandle,
     };
     SecureBufferType mSecureBufferType[2];
-    bool mGraphicBufferEnabled[2];
 
     // Following are OMX parameters managed by us (instead of the component)
     // OMX_IndexParamMaxFrameDurationForBitrateControl
