aff85165eee3c3853a3b2160650a14cf0895b9e6
Shilin Victor <chrono.monochrome@gmail.com>
2018-09-10 09:46:34 +0300

    Revert "libaudiohal: reset to lineage-15.1"
    This reverts commit 91543771a734ee3fcb160b36c96b47eec5c48d4d.
    
    Change-Id: I2f6c09da93d2a0b834fe73e5a90848bf614cecc0

--- media/libaudiohal/4.0/DeviceHalHidl.h
+++ media/libaudiohal/4.0/DeviceHalHidl.h
@@ -14,56 +14,58 @@
  * limitations under the License.
  */
 
-#ifndef ANDROID_HARDWARE_DEVICE_HAL_INTERFACE_H
-#define ANDROID_HARDWARE_DEVICE_HAL_INTERFACE_H
+#ifndef ANDROID_HARDWARE_DEVICE_HAL_HIDL_4_0_H
+#define ANDROID_HARDWARE_DEVICE_HAL_HIDL_4_0_H
 
-#include <media/MicrophoneInfo.h>
-#include <system/audio.h>
-#include <utils/Errors.h>
-#include <utils/RefBase.h>
-#include <utils/String8.h>
+#include <android/hardware/audio/4.0/IDevice.h>
+#include <android/hardware/audio/4.0/IPrimaryDevice.h>
+#include <media/audiohal/DeviceHalInterface.h>
+
+#include "ConversionHelperHidl.h"
+
+using ::android::hardware::audio::V4_0::IDevice;
+using ::android::hardware::audio::V4_0::IPrimaryDevice;
+using ::android::hardware::Return;
 
 namespace android {
+namespace V4_0 {
 
-class StreamInHalInterface;
-class StreamOutHalInterface;
-
-class DeviceHalInterface : public RefBase
+class DeviceHalHidl : public DeviceHalInterface, public ConversionHelperHidl
 {
   public:
     // Sets the value of 'devices' to a bitmask of 1 or more values of audio_devices_t.
-    virtual status_t getSupportedDevices(uint32_t *devices) = 0;
+    virtual status_t getSupportedDevices(uint32_t *devices);
 
     // Check to see if the audio hardware interface has been initialized.
-    virtual status_t initCheck() = 0;
+    virtual status_t initCheck();
 
     // Set the audio volume of a voice call. Range is between 0.0 and 1.0.
-    virtual status_t setVoiceVolume(float volume) = 0;
+    virtual status_t setVoiceVolume(float volume);
 
     // Set the audio volume for all audio activities other than voice call.
-    virtual status_t setMasterVolume(float volume) = 0;
+    virtual status_t setMasterVolume(float volume);
 
     // Get the current master volume value for the HAL.
-    virtual status_t getMasterVolume(float *volume) = 0;
+    virtual status_t getMasterVolume(float *volume);
 
     // Called when the audio mode changes.
-    virtual status_t setMode(audio_mode_t mode) = 0;
+    virtual status_t setMode(audio_mode_t mode);
 
     // Muting control.
-    virtual status_t setMicMute(bool state) = 0;
-    virtual status_t getMicMute(bool *state) = 0;
-    virtual status_t setMasterMute(bool state) = 0;
-    virtual status_t getMasterMute(bool *state) = 0;
+    virtual status_t setMicMute(bool state);
+    virtual status_t getMicMute(bool *state);
+    virtual status_t setMasterMute(bool state);
+    virtual status_t getMasterMute(bool *state);
 
     // Set global audio parameters.
-    virtual status_t setParameters(const String8& kvPairs) = 0;
+    virtual status_t setParameters(const String8& kvPairs);
 
     // Get global audio parameters.
-    virtual status_t getParameters(const String8& keys, String8 *values) = 0;
+    virtual status_t getParameters(const String8& keys, String8 *values);
 
     // Returns audio input buffer size according to parameters passed.
     virtual status_t getInputBufferSize(const struct audio_config *config,
-            size_t *size) = 0;
+            size_t *size);
 
     // Creates and opens the audio hardware output stream. The stream is closed
     // by releasing all references to the returned object.
@@ -73,7 +75,7 @@ class DeviceHalInterface : public RefBase
             audio_output_flags_t flags,
             struct audio_config *config,
             const char *address,
-            sp<StreamOutHalInterface> *outStream) = 0;
+            sp<StreamOutHalInterface> *outStream);
 
     // Creates and opens the audio hardware input stream. The stream is closed
     // by releasing all references to the returned object.
@@ -84,10 +86,10 @@ class DeviceHalInterface : public RefBase
             audio_input_flags_t flags,
             const char *address,
             audio_source_t source,
-            sp<StreamInHalInterface> *inStream) = 0;
+            sp<StreamInHalInterface> *inStream);
 
     // Returns whether createAudioPatch and releaseAudioPatch operations are supported.
-    virtual status_t supportsAudioPatches(bool *supportsPatches) = 0;
+    virtual status_t supportsAudioPatches(bool *supportsPatches);
 
     // Creates an audio patch between several source and sink ports.
     virtual status_t createAudioPatch(
@@ -95,30 +97,35 @@ class DeviceHalInterface : public RefBase
             const struct audio_port_config *sources,
             unsigned int num_sinks,
             const struct audio_port_config *sinks,
-            audio_patch_handle_t *patch) = 0;
+            audio_patch_handle_t *patch);
 
     // Releases an audio patch.
-    virtual status_t releaseAudioPatch(audio_patch_handle_t patch) = 0;
+    virtual status_t releaseAudioPatch(audio_patch_handle_t patch);
 
     // Fills the list of supported attributes for a given audio port.
-    virtual status_t getAudioPort(struct audio_port *port) = 0;
+    virtual status_t getAudioPort(struct audio_port *port);
 
     // Set audio port configuration.
-    virtual status_t setAudioPortConfig(const struct audio_port_config *config) = 0;
+    virtual status_t setAudioPortConfig(const struct audio_port_config *config);
 
     // List microphones
-    virtual status_t getMicrophones(std::vector<media::MicrophoneInfo> *microphones) = 0;
+    virtual status_t getMicrophones(std::vector<media::MicrophoneInfo> *microphones);
 
-    virtual status_t dump(int fd) = 0;
+    virtual status_t dump(int fd);
 
-  protected:
-    // Subclasses can not be constructed directly by clients.
-    DeviceHalInterface() {}
+  private:
+    friend class DevicesFactoryHalHidl;
+    sp<IDevice> mDevice;
+    sp<IPrimaryDevice> mPrimaryDevice;  // Null if it's not a primary device.
+
+    // Can not be constructed directly by clients.
+    explicit DeviceHalHidl(const sp<IDevice>& device);
 
     // The destructor automatically closes the device.
-    virtual ~DeviceHalInterface() {}
+    virtual ~DeviceHalHidl();
 };
 
+} // namespace V4_0
 } // namespace android
 
-#endif // ANDROID_HARDWARE_DEVICE_HAL_INTERFACE_H
+#endif // ANDROID_HARDWARE_DEVICE_HAL_HIDL_4_0_H
